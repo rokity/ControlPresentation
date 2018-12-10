fbb73643bec64308939f75a6bf7fef6cbc317098
rokity <riccardo.amadio1@gmail.com>
2018-12-10 20:06:38 +0100

    change the architecture of the repo and complete the task about ask for the server port I divide the index.js in two part: one manage the http-server inside lib/http-server.js, the other part manage the websocket-server and it's inside lib/websocket-server.js Now there are two modules websocket and http server. In the main file I used this two modules to create the http server and websocket server. I'm using the readline native module of NodeJS to interact with the user and ask for server port of HTTP and WebSocket.

--- index.js
+++ index.js
@@ -1,73 +1,25 @@
-const http = require('http');
-const fs = require('fs');
-const robot = require("robotjs");
-const qrcode = require('qrcode-terminal');
-const network = require('network');
 const pino = require('pino');
-const WebSocketServer = require('websocket').server;
-// Get the source of Control Page
-const html = fs.readFileSync('index.html', 'utf8');
+const readline = require('readline');
 //Initiate the PinoJS logger library,adding prettify
-const logger = pino({ prettyPrint: { colorize: true } });
-/** 
- * Create a HTTP server object
- * It's the response to every HTTP Request 
- * of this server.
-*/
-let server = http.createServer((req,res) => {
-    //Set HTTP Status Code
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    //write the html response
-    res.write(html);
-    //close the HTTP Connection
-    res.end();
-})
-/**
- * Catch the case if the server is already in use or started.
- */
-server.on('error', (e) => {
-    if (e.code === 'EADDRINUSE') {
-        logger.fatal(`
-      ERROR: Address in use
-      Maybe there are some service 
-      that actually working in the same port.
-      Check it and re-launch the app.  `);
-    }
-    process.exit(0);
+global.logger = pino({ prettyPrint: { colorize: true } });
+//Http-Server module /lib/http-server.js
+let http_server = require(`./lib/http-server.js`);
+//WebSocket-Server module /lib/websocket-server.js
+let websocket_server = require(`./lib/websocket-server.js`);
+//Init readline interface for prompt question and get input data
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
-/**
- * Launch the HTTP Server .
- * Get the private IP of the host.
- * Generate the QR Code from the url of the http-server and show it.
- */
-server.listen(8080, function () {
-    logger.info(' Server is listening on port 8080 ');
-    network.get_private_ip(function (err, ip) {
-        const private_address = `http://${ip}:8080`;
-        qrcode.generate(private_address);
-    })
+//Ask question about port of the server,
+// http and websocket server are using the same port
+
+rl.question('Which port do you want to use for the server? (8080) :', (port) => {
+    port = port.length > 0 ? port : 8080;
+    rl.close();
+    //Start HTTP-Server and WebSocket-Server
+    http_server.start(port);
+    websocket_server.start(http_server.server);
 });
-/**
- * Create the WebSocket Server from HTTP Server.
- * The autoaccept property is disabled because 
- * it's more secure for production enivronment.
- */
-let wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false
-});
-/**
- * Event for managing the WebSocket Request.
- * Accept the request and on a new message 
- * write on the console the message.
- * Call RobotJS library which trigger 
- * the left/right/up/down button event.
- * The message could be 'left'|'right'|'up'|'down'.
- */
-wsServer.on('request', function (request) {
-    var connection = request.accept(null, request.origin);
-    connection.on('message', function (message) {
-        logger.info(message);
-        robot.keyTap(message.utf8Data);
-    });
-});
+
+
